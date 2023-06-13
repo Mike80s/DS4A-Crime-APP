a56fce4452e9102674e89434f479826bb59ba846
Michael Fabián Ramos Hernández <108836758+Mike80s@users.noreply.github.com>
2023-06-13 07:53:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 
 # from sklearn.preprocessing import scale
-
+# This is gonna be awesome
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 meta_tags=[{'name':'viewport',
